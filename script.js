00c204d330399b4e3e9d2e37c6002e89837ec39e
samis <samilsully@gmail.com>
2020-09-12 18:06:56 -0700

    attached the CSS to each component and added a click listener to store typed data into local storage

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+// checks if the document is ready first before displaying everything!
+$(document).ready(function () {
+  console.log("ready!");
+});
 // Variable to pull and set the time at the top of the planner
 var currentDate = moment().format("dddd, MMMM do, LT");
+var currentHour = moment().hours();
 console.log(currentDate);
-// Variables to pull from to set the time
-var numericalHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+console.log(currentHour);
+// Variables to pull from to set the time values and the numerial set that assigns each row in order to p
+var numericalHours = [09, 10, 11, 12, 13, 14, 15, 16, 17];
 var visibleHours = [
   "9:00 AM",
   "10:00 AM",
@@ -14,8 +20,10 @@ var visibleHours = [
   "4:00 PM",
   "5:00 PM",
 ];
-
+// sets the current date at the top of the page
 $("#currentDay").text(currentDate);
+
+var divRow = $("<div></div>");
 
 // create for loop that will run through the hours to create each time block
 function timeblockBuilder() {
@@ -23,36 +31,47 @@ function timeblockBuilder() {
   for (i = 0; i < visibleHours.length; i++) {
     var divRow = $("<div></div>");
     divRow.addClass("row");
-
+    // creates the hours list to the left side of the main content that will correlate what our it is to the text.
     $(divRow).append(
       $("<div/>")
-        .attr("id", `${numericalHours[i]}`)
-        .addClass("col-sm-2 hour")
+        .attr("data-time", $[numericalHours[i]])
+        .addClass("col-sm-1 hour")
         .append("<p/>")
-        .text(`${visibleHours[i]}`)
+        .text([visibleHours[i]])
     );
-
-    $(divRow).append($("<div/>").addClass("col-sm-8").append("<textarea/>"));
-
-    $(divRow).append(
-      $("<div/>").addClass("col-sm-2 saveBtn").append("<button/>").text(`save`)
+    //creates the text area for the user to input their daily tasks
+    var newTextArea = $("<textarea/>")
+      .addClass("col-sm-10 description")
+      .attr("data-time", numericalHours[i]);
+    $(divRow).append(newTextArea);
+    // runs the if statement to check if there is any stored info. if there is, it places it into the correct text areas corresponding the hour
+    if (localStorage.getItem(numericalHours[i])) {
+      newTextArea.text(localStorage.getItem(numericalHours[i]));
+    }
+    // creates the buttons that will be generated on the right side of the planner
+    var btnArea = $("<button class='col-sm-1 saveBtn far fa-save'>").attr(
+      "data-time",
+      numericalHours[i]
     );
-
+    $(divRow).append(btnArea);
+    //   appends all of the created rows into the main container
     $("#full").append(divRow);
+
+    //    checks the time for the user and places the correct CSS for the current timeframe.
+    if (currentHour == numericalHours[i]) {
+      newTextArea.addClass("present");
+    } else if (numericalHours[i] < currentHour) {
+      newTextArea.addClass("past");
+    } else {
+      newTextArea.addClass("future");
+    }
   }
 }
+// Saves the text the user inputted to local storage
+$(document).on("click", ".saveBtn", function (event) {
+  var storedHour = $(this).attr("data-time");
+  var storedText = $(this).siblings(".description").val();
+  localStorage.setItem(storedHour, storedText);
+});
+// Runs the timeblock builder to create the time blocks with MomentJS !
 timeblockBuilder();
-// not sure if I want to dynamically create columns, or should I have the culumns preset?
-// needs to contain button for saving the text on the right side
-// needs to contain time on th left side
-// intractive text that saves to local storage in the middle
-
-// write function that will color each timeblock for past, present, and future
-
-{
-  /* <div class="row">
-  <div class="col-sm-2" id="#hour"></div>
-  <div class="col-sm-8" id=""></div>
-  <div class="col-sm-2" id="#saveBtn"></div>
-</div>; */
-}
